2187cf06e9ac96927faaa46ce27debf1ddc63fa3
Diana <diana_nunez10@yahoo.com>
2021-06-09 15:46:06 -0700

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+    #name can't be blank
+    #e-mail is unique
+    validates :name, presence: true
+    validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    #title can't be blank
+    #category either "Fiction" or "Non-Fiction"
+    #content min 100 chars
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
 end
